9e5dfa965899ce8a0970429dff6379932ba64381
Mehtab Hussain <tradercoder786@gmail.com>
2025-06-29 08:32:55 +0530

    made necessary changes

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+require('dotenv').config();
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const cors = require("cors");
+
+server.use(cors());
+server.use(middlewares);
+server.use("/api", router); // optional prefix
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
